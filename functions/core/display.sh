8836d8dfea50b7d2874e91ab9af40af484eab910
amogus <hi@amogusreal.tech>
2025-12-14 11:38:06 +0000

    feat: add memory

--- functions/core/display.sh
+++ functions/core/display.sh
@@ -4,7 +4,7 @@ function display {
     echo -e "\e[38;2;195;144;230m\e[4mHardware Information\e[0m"
     echo -e "\e[38;2;195;144;230m  Architecture: \e[38;5;250m$ARCH\e[0m"
     echo -e "\e[38;2;195;144;230m  CPU: \e[38;5;250m$(lscpu | grep 'Model name' | awk -F: '{print $2}' | sed 's/^[ \t]*//')\e[0m"
-    echo -e "\e[38;2;195;144;230m  Memory: \e[38;5;250m$SERVER_MEMORY MB\e[0m"
+    echo -e "\e[38;2;195;144;230m  Memory: \e[90m$(free | awk '/^Mem:/ {printf "%.2f GiB out of %.2f GiB", $3/1024/1024, $2/1024/1024}')\e[0m  \e[92m$(free | awk '/^Mem:/ {printf "(%.2f%%)", $3/$2*100}')\e[0m"
     echo -e "\e[38;2;195;144;230m  Location: \e[38;5;250m$P_SERVER_LOCATION\e[0m"
     echo -e "\e[38;2;195;144;230m  Server Port: \e[38;5;250m$SERVER_PORT\e[0m"
     echo -e "\e[1;36m \e[0m"
